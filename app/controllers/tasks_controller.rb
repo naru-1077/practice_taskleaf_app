7e8023c63426da917409b2a8e3798ce5e694ca15
naru-1077 <t.n.narunaru@gmail.com>
2021-06-25 07:32:55 +0000

    [Add]新規タスク登録 flashメッセージ追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,7 +10,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    task = Task.new(task_params)
     task.save!
     redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
   end
